8eadce85640e4d14c33c737e40cce85598d2f4f2
EugeneKhokhlov322 <6918598@stud.kai.edu.ua>
2025-05-11 14:38:24 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,89 +1,67 @@
 ﻿#include <iostream>
-
+#include <cmath>
+#include <stdexcept>
 using namespace std;
 
 class Operation {
 public:
+    virtual ~Operation() = default;
     virtual double execute(double a, double b) = 0;
-};
-
-class Addition : public Operation {
-public:
-    double execute(double a, double b) override {
-        return a + b;
-    }
-};
-
-class Subtraction : public Operation {
-public:
-    double execute(double a, double b) override {
-        return a - b;
-    }
-};
-
-class Multiplication : public Operation {
-public:
-    double execute(double a, double b) override {
-        return a * b;
-    }
-};
-
-class Division : public Operation {
-public:
-    double execute(double a, double b) override {
-        if (b == 0) {
-            throw invalid_argument("Error: division by zero!");
-        }
-        return a / b;
-    }
 };
 
 class Calculator {
 private:
     Operation* operation;
+    double memory = 0; // Додано поле пам'яті
 
 public:
     void setOperation(Operation* op) {
         operation = op;
     }
 
-    double calculate(double a, double b) {
-        return operation->execute(a, b);
+    double calculate(double a, double b, bool log = false) {
+        double result = operation->execute(a, b);
+        if (log) {
+            cout << "Logged: " << a << " op " << b << " = " << result << endl;
+        }
+        return result;
+    }
+
+    // Нові методи для роботи з пам'яттю
+    void storeToMemory(double value) {
+        memory = value;
+    }
+
+    double recallFromMemory() const {
+        return memory;
     }
 };
 
-class Power : public Operation {
+// Класи операцій з вбудованими тимчасовими змінними
+class Addition : public Operation {
 public:
     double execute(double a, double b) override {
-        return pow(a, b);
+        return a + b; // Вбудована тимчасова змінна
+    }
+};
+
+class Division : public Operation {
+public:
+    double execute(double a, double b) override {
+        if (b == 0) throw invalid_argument("Division by zero");
+        return a / b; // Вбудована тимчасова змінна
     }
 };
 
 int main() {
     Calculator calc;
     Addition add;
-    Subtraction sub;
-    Multiplication mul;
-    Division div;
-    Power pow;
-
-    double a = 10;
-    double b = 5;
 
     calc.setOperation(&add);
-    cout << "Addition: " << calc.calculate(a, b) << endl;
+    cout << "10 + 5 = " << calc.calculate(10, 5, true) << endl;
 
-    calc.setOperation(&sub);
-    cout << "Subtraction: " << calc.calculate(a, b) << endl;
-
-    calc.setOperation(&mul);
-    cout << "Multiplication: " << calc.calculate(a, b) << endl;
-
-    calc.setOperation(&div);
-    cout << "Division: " << calc.calculate(a, b) << endl;
-
-    calc.setOperation(&pow);
-    cout << "Power: " << calc.calculate(a, b) << endl;
+    calc.storeToMemory(100);
+    cout << "Memory: " << calc.recallFromMemory() << endl;
 
     return 0;
 }
